2bdbfa9d0d7eaccaa94a65676e36c39a2f3090fc
Effy <fatimam2000@gmail.com>
2025-12-30 12:25:31 -0500

    Initial Commit

---
+++ CameraTestTests/CameraTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  CameraTestTests.swift
+//  CameraTestTests
+//
+//  Created by Effy on 2025-12-30.
+//
+
+import Testing
+@testable import CameraTest
+
+struct CameraTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
