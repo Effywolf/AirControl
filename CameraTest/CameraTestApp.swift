3ca70573c5ecb3bbb9ef6fbfd4ec3805ab0a9bb7
Effy <fatimam2000@gmail.com>
2025-12-30 20:39:16 -0500

    u

--- CameraTest/CameraTestApp.swift
+++ CameraTest/CameraTestApp.swift
@@ -12,8 +12,6 @@ class CameraTestApp: NSObject, NSApplicationDelegate {
 
     private var gestureController: GestureController!
     private var menuBarController: MenuBarController!
-
-    // Keep strong reference to prevent deallocation
     private static var appDelegate: CameraTestApp?
 
     static func main() {
@@ -24,21 +22,16 @@ class CameraTestApp: NSObject, NSApplicationDelegate {
     }
 
     func applicationDidFinishLaunching(_ notification: Notification) {
-        // Initialize controllers
+        
         gestureController = GestureController()
         menuBarController = MenuBarController()
-
-        // Setup menu bar
         menuBarController.setup(with: gestureController)
-
-        // Start gesture control automatically
         gestureController.start()
 
         print("Gesture Control app started")
     }
 
     func applicationWillTerminate(_ notification: Notification) {
-        // Clean up
         gestureController.stop()
         print("Gesture Control app terminated")
     }
